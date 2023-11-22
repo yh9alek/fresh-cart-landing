b9c16193d56d1557a39676e9336100c20023de49
Yohan Alek <yh9dev@gmail.com>
2023-11-21 17:17:34 -0700

    The header section was finished, but the left menu still need´s to be designed when viewport width get 865px

--- views/_layout.php
+++ views/_layout.php
@@ -6,8 +6,10 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Fresh Cart</title>
-        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1129195909796860029/1176623376576561162/icon.PNG?ex=656f8ae9&is=655d15e9&hm=759e32a1a0d497573b6f3b13068180a75d93a8a64f8aecb794ea0206aeb305bb&">
         <link rel="stylesheet" href="/assets/css/home.css">
+        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1129195909796860029/1176623376576561162/icon.PNG?ex=656f8ae9&is=655d15e9&hm=759e32a1a0d497573b6f3b13068180a75d93a8a64f8aecb794ea0206aeb305bb&">
+        <!-- Font Awesome CSS link -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
     <body>
         <?= $content ?>
--- views/products/home.php
+++ views/products/home.php
@@ -1,13 +1,45 @@
 <header>
     <div class="header__container container">
-        <div class="header__title">
-            <figure class="title__figure">
-                <img src="https://cdn.discordapp.com/attachments/1129195909796860029/1176623376576561162/icon.PNG?ex=656f8ae9&is=655d15e9&hm=759e32a1a0d497573b6f3b13068180a75d93a8a64f8aecb794ea0206aeb305bb&" class="figure__img">
-            </figure>
-            <h3 class="title">FreshCart</h3>
+        <div class="header__r1">
+            <div class="header__title">
+                <figure class="title__figure">
+                    <img src="https://cdn.discordapp.com/attachments/1129195909796860029/1176623376576561162/icon.PNG?ex=656f8ae9&is=655d15e9&hm=759e32a1a0d497573b6f3b13068180a75d93a8a64f8aecb794ea0206aeb305bb&" class="figure__img">
+                </figure>
+                <h3 class="title">FreshCart</h3>
+            </div>
+            <div class="header__mid">
+                <form class="search__container" method="post">
+                    <input name="search" class="header__search form__control" type="search" placeholder="Search for products">
+                    <button type="submit" class="header__search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+                <button type="submit" class="header__location-btn form__control"><i class="fa-solid fa-location-dot"></i> Location</button>
+            </div>
+            <div class="header__btns">
+                <i class="fa-regular fa-heart"></i>
+                <i class="fa-regular fa-user"></i>
+                <i class="fa-solid fa-bag-shopping"></i>
+                <button class="menu__btn active"><img src="https://cdn.discordapp.com/attachments/1129195909796860029/1176675266634985542/menu.PNG?ex=656fbb3d&is=655d463d&hm=fa9514fc71a859f2a36acd02f93d1a79fef848a70f4c77b71b83fd4dde9f6458&"></button>
+            </div>
         </div>
-        <form method="post">
-            <input class="header__search form-control" type="search" placeholder="Search for products">
-        </form>
+        <div class="header__r2">
+            <button class="header__departments-btn form__control">All Departaments</button>
+            <div class="header__selects">
+                <select class="header__select header__home form__control">
+                    <option>Home</option>
+                </select>
+                <select class="header__select header__shop form__control">
+                    <option>Shop</option>
+                </select>
+                <select class="header__select header__stores form__control">
+                    <option>Stores</option>
+                </select>
+                <select class="header__select header__menu form__control">
+                    <option>Mega Menu</option>
+                </select>
+                <select class="header__select header__pages form__control">
+                    <option>Pages</option>
+                </select>
+            </div>
+        </div>
     </div>
 </header>
