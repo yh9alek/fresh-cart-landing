693d96bdd46c92ebd83f29328e00d3c905494566
Yohan Alek <yh9dev@gmail.com>
2023-11-24 16:48:27 -0700

    The shorticon was changed

--- views/_layout.php
+++ views/_layout.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Fresh Cart</title>
         <link rel="stylesheet" href="/assets/css/home.css">
-        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1129195909796860029/1176623376576561162/icon.PNG?ex=656f8ae9&is=655d15e9&hm=759e32a1a0d497573b6f3b13068180a75d93a8a64f8aecb794ea0206aeb305bb&">
+        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1129195909796860029/1177757899800465429/icon.png?ex=6573ab84&is=65613684&hm=be8d91607f53ee0aff36df26d6e45a7ea3ae164f6b248fb10ea6181f918e7cfb&">
         <!-- Font Awesome CSS link -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- Bootstrap Icons link -->
